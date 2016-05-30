f8346447ee3389b061a5f875ba9f49c227000e0b
Kyle Laughlin <kylejlaughlin@icloud.com>
2016-05-30 11:09:38 -0500

    add all files for user and sessions

--- Gemfile
+++ Gemfile
@@ -17,7 +17,8 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
-
+# Sorcery for user authentication https://github.com/NoamB/sorcery
+gem "sorcery"
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   root to: 'static_pages#home'
+
+  resources :user_sessions
+  resources :users
+
+  get 'login' => 'user_sessions#new', :as => :login
+  post 'logout' => 'user_sessions#destroy', :as => :logout
 
 
   # You can have the root of your site routed with "root"
