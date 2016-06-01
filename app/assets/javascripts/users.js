5246e840507e6dfac77740042652121a2d5c50b1
Kyle Laughlin <kylejlaughlin@icloud.com>
2016-06-01 15:25:52 -0500

    clean up user.js

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -2,12 +2,14 @@
 // All this logic will automatically be available in application.js.
 
 $(function(){
-
+  
+  // initialize an email to be shared
   function shareEmail(name, email){
     var self = this;
     this.email = email;
   }
 
+  // Viewmodel
   function userModel() {
     var self = this;
     self.firstName = ko.observable();
@@ -16,6 +18,7 @@ $(function(){
     self.userId = ko.observable();
     self.myEmails = ko.observableArray([]);
 
+    // Send Images via AJAX to server
     self.uploadImage = function(){
       var fileSelect = document.getElementById('file-selected');
       var files = fileSelect.files;
@@ -41,9 +44,12 @@ $(function(){
       });
     }
 
+    // Display user info edit form
     self.showEdit = function(){
       $("#user-edit").hide().removeClass("display-hide").slideDown(600);
     }
+
+    // Upon submit click - Hide user info edit form and send info to server
     self.hideEdit = function(){
       var postData = {first_name: self.firstName, last_name: self.lastName, email: self.email}
       $.ajax({
@@ -54,24 +60,29 @@ $(function(){
       $("#user-edit").slideUp(200);
     }
 
+    // Hide Images and show email sharing
     self.showShare = function(){
       $("#image-show").addClass("display-hide");
       $("#email-share").removeClass("display-hide");
     }
 
+    // Hide email sharing and show images
     self.showImages = function(){
       $("#email-share").addClass("display-hide");
       $("#image-show").removeClass("display-hide");
     }
 
+    // Add another email to the myEmails ko.observableArray
     self.addEmail = function(){
       self.myEmails.push(new shareEmail("", ""));
     }
 
+    // Remove an email from the myEmails ko.observableArray
     self.removeEmail = function(email){
       self.myEmails.remove(email);
     }
 
+    // Package all share emails into a dataObject to be sent via AJAX to server
     self.sendEmail = function(){
       var emailList = [];
       for(var i = 0; i < self.myEmails().length; i++){
@@ -89,6 +100,7 @@ $(function(){
       });
     }
 
+    // Populate iniital observable values
     $.get('/users/current', function(data){
       self.firstName(data.first_name);
       self.lastName(data.last_name);
@@ -101,6 +113,7 @@ $(function(){
 
   }
 
+  // Instantiate ko bindings
   ko.applyBindings(new userModel());
 
 });
